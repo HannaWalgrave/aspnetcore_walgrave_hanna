d68a06f86a5bd805ad5aee8313c25d6bee98c9d5
Hanna Walgrave <Walgrave.hanna@hotmail.com>
2017-09-24 15:08:43 +0200

    initial commit

---
+++ aspnetcore_walgrave_hanna/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aspnetcore_walgrave_hanna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
